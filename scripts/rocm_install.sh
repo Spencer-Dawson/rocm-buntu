166635e89742f9f135c01170dcd0848e08846242
spencer.dawson <spec4d@gmail.com>
2024-01-04 16:50:20 -0700

    Added support for rocm 6.0 installation

--- scripts/rocm_install.sh
+++ scripts/rocm_install.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-echo "This script is completely untested. "
-echo "It's more intended as a general overview of what needs to be done to install ROCm on Ubuntu 22.04 then as an actual installer. "
-echo "Use at your own risk. And provide feedback if you have suggested fixes. "
-
 # make sure user wants to continue
 read -p "Do you want to continue? [y/N] " -n 1 -r
 echo
@@ -29,17 +25,32 @@ fi
 
 # Ask user to specify the version of ROCm to install
 echo "Which version of ROCm do you want to install?"
-echo "1) ROCm 5.4"
-echo "2) ROCm 5.3"
+echo "1) ROCM 6.0"
+echo "2) ROCm 5.7"
+echo "3) ROCm 5.6"
+echo "4) ROCm 5.5"
+echo "5) ROCm 5.4"
+echo "6) ROCm 5.3"
+echo "7) ROCm 5.2"
 
 read -p "Input> " rocm_version
 rocm_version=${rocm_version:0:1}
 
 # Convert rocm_version to the actual version number
 if [ "$rocm_version" == "1" ]; then
-    rocm_version="5.4"
+    rocm_version="6.0"
 elif [ "$rocm_version" == "2" ]; then
+    rocm_version="5.7"
+elif [ "$rocm_version" == "3" ]; then
+    rocm_version="5.6"
+elif [ "$rocm_version" == "4" ]; then
+    rocm_version="5.5"
+elif [ "$rocm_version" == "5" ]; then
+    rocm_version="5.4"
+elif [ "$rocm_version" == "6" ]; then
     rocm_version="5.3"
+elif [ "$rocm_version" == "7" ]; then
+    rocm_version="5.2"
 else
     echo "Invalid choice. Exiting..."
     exit 1
@@ -51,20 +62,34 @@ cd /tmp/rocm || exit 1
 
 sudo apt update || exit 1
 
-# Install ROCm5.4 if user specified
-if [ "$rocm_version" == "5.4" ]; then
+if [ "$rocm_version" == "6.0" ]; then
+    echo "Installing ROCm 6.0"
+    wget https://repo.radeon.com/amdgpu-install/6.0/ubuntu/jammy/amdgpu-install_6.0.60000-1_all.deb || exit 1
+    sudo sudo dpkg -i ./amdgpu-install_6.0.60000-1_all.deb || exit 1
+elif [ "$rocm_version" == "5.7" ]; then
+    echo "Installing ROCm 5.7"
+    wget https://repo.radeon.com/amdgpu-install/5.7/ubuntu/jammy/amdgpu-install_5.7.50700-1_all.deb || exit 1
+    sudo sudo dpkg -i ./amdgpu-install_5.7.50700-1_all.deb || exit 1
+elif [ "$rocm_version" == "5.6" ]; then
+    echo "Installing ROCm 5.6"
+    wget https://repo.radeon.com/amdgpu-install/5.6/ubuntu/jammy/amdgpu-install_5.6.100-1236248_all.deb || exit 1
+    sudo sudo dpkg -i ./amdgpu-install_5.6.100-1236248_all.deb || exit 1
+elif [ "$rocm_version" == "5.5" ]; then
+    echo "Installing ROCm 5.5"
+    wget https://repo.radeon.com/amdgpu-install/5.5/ubuntu/jammy/amdgpu-install_5.5.100-1236248_all.deb || exit 1
+    sudo sudo dpkg -i ./amdgpu-install_5.5.100-1236248_all.deb || exit 1
+elif [ "$rocm_version" == "5.4" ]; then
     echo "Installing ROCm 5.4"
     wget https://repo.radeon.com/amdgpu-install/5.4.3/ubuntu/jammy/amdgpu-install_5.4.50403-1_all.deb || exit 1
-    sudo apt-get install ./amdgpu-install_5.4.50403-1_all.deb || exit 1
-
-fi
-
-# Install ROCm5.3 if user specified
-if [ "$rocm_version" == "5.3" ]; then
+    sudo sudo dpkg -i ./amdgpu-install_5.4.50403-1_all.deb || exit 1
+elif [ "$rocm_version" == "5.3" ]; then
     echo "Installing ROCm 5.3"
     wget https://repo.radeon.com/amdgpu-install/5.3/ubuntu/jammy/amdgpu-install_5.3.50300-1_all.deb || exit 1
-    sudo apt install ./amdgpu-install_5.3.50300-1_all.deb || exit 1
-    exit 1
+    sudo sudo dpkg -i ./amdgpu-install_5.3.50300-1_all.deb || exit 1
+elif [ "$rocm_version" == "5.2" ]; then
+    echo "Installing ROCm 5.2"
+    wget https://repo.radeon.com/amdgpu-install/22.20.1/ubuntu/bionic/amdgpu-install_22.20.50201-1_all.deb || exit 1
+    sudo sudo dpkg -i ./amdgpu-install_22.20.50201-1_all.deb || exit 1
 fi
 
 # ensure amdgpu-install is configured properly for ROCm usecase
